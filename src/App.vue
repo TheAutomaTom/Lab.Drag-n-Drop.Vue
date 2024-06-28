e2a34269b93b76f1a4eb0b20918f6f5bfa75977a
Tom Grossi <TheAutomaTom@gmail.com>
2024-06-28 12:05:37 -0500

    W3 demo code

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,36 @@
 <script setup lang="ts">
 
+function allowDrop(ev: any) {
+  ev.preventDefault();
+}
+
+function drag(ev: any) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev: any) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
 </script>
 
 <template>
-  
-Work in progress...
+
+<div 
+  id="div1" 
+  ondrop="drop(event)" 
+  ondragover="allowDrop(event)"
+>[]</div>
+
+<img 
+  id="drag1" 
+  src="../public/img_logo.gif"
+  draggable="true" 
+  ondragstart="drag(event)"
+  width="70" 
+  height="70"
+/>  
 
 
 </template>
